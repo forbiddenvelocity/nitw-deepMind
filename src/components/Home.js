5e4fcf41eae15e9b3bcfccc3f71c4aed5d243334
Mubashir <mubashir1728@gmail.com>
2023-10-06 01:54:34 +0530

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const Home = () => {
+  return (
+    <>
+    <div className='about-team-contact-join'>
+        <div>Home</div><div>Team</div><div>Contact</div><div>Join</div>
+    
+    </div>
+    
+    <div className='community-description'> A Self-driven Artificial Intelligence and Machine Learning student community of NIT Warangal by EA and Ham Club. 
+    </div>
+
+    <div className='nitw-deepmind'> NITW deepMind</div>
+
+    <div className='pattern-1'></div>
+
+    <div className='rectangle-1'></div>
+
+    <div className='welcome-message'> Welcome to the AI-ML Student Community at NIT Warangal </div>
+
+    <div className='unlock-potential-message'>Unlock Your Potential with the Power of AI and ML Are you ready to embark on a journey of innovation and discovery? Join our vibrant community of self-driven passionate students at NIT Warangal exclusively in AI-ML domain, brought to you by EA and Ham Club.</div>
+
+    <div className='star'>*</div>
+
+    <div className='image-1'></div>
+    </>
+
+  )
+}
+
+export default Home
