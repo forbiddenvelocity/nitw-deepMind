cf83a7f1a49bec42154b9830dfaf1f0d8fd82a93
Mubashir <mubashir1728@gmail.com>
2023-10-06 21:56:19 +0530

    changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ import Navbar from './Navbar'
 const Home = () => {
   return (
     <>
+    <div className='container'></div>
     <div className='about-team-contact-join'>
         <div>Home</div><div>Team</div><div>Contact</div><div>Join</div>
     
@@ -26,8 +27,41 @@ const Home = () => {
     <div className='star'>*</div>
 
     <div className='image-1'></div>
-
+    <div className='line-1'></div>
+    <a href="https://github.com/forbiddenvelocity/Image-Generator-AI" target="_blank">
+  <div className='gan-text'>
+    GAN Image Generator
+  </div>
+</a>
     <div className='projects'>Projects</div>
+    
+    <div className='project-1'></div>
+    <div className='projects-text'> We actively undertake several personal passion projects. </div>
+    
+    <div className='project-2'></div>
+    <a href="https://github.com/forbiddenvelocity/deepArt-SynapseAI" target="_blank"><div className='deepart'>DeepArt - deep Dreaming project by Synapse.ai</div></a>
+    
+    <div className='project-3'></div>
+    <div className='project-5'></div>
+    <div className='project-6'></div>
+
+    <div className='line-2'></div>
+    <div className='research-container'>Research</div>
+    <div className='research-papers'>Some of the research papers published by our student members.</div>
+    <div className='line-3'></div>
+    <div className='andrew-container'></div>
+    <div className='andrew-quote'>“No one knows what the right
+algorithm is but it gives us hope that if we can discover some crude approximation of whatever this algorithm is and implement on a computer, that can help us make a lot of progress. “ 
+
+- Andrew Ng</div>
+    <div className='line-4'></div>
+    <div className='join-us'>Join us in changing the Machine Learning culture at NITW</div>
+    <div className='deepmind'>NITW deepMind <br></br> EA & Ham</div>
+    <div className='line-5'></div>
+    <div className='deepmind-copyright'>NITW deepMind © 2023</div>
+    <div className='brand-guidelines'>Terms and Policies <br></br> Brand Guidelines</div>
+    <div className='social-icons'>GitHub YouTube Instagram</div>
+    <div className='rectangle-2'></div>
     </>
 
   )
